a19567104db7370c9b048f1b824efa13ff09cc0d
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2020-05-19 23:08:06 -0600

    Add spacing around module docstring

--- ccd2iso/clonecd.py
+++ ccd2iso/clonecd.py
@@ -1,7 +1,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 """C Structure representation of the CloneCD .img format."""
+
 from ctypes import c_ubyte, Structure, Union
 
 DATA_SIZE = 2048
