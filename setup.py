da9511cc64d1424b27df2df7b2648e7c6a11c7fd
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2020-05-19 21:20:12 -0600

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='ccd2iso',
+    version='0.0.1',
+    description='Command-line utility to convert a CloneCD .img file to a .iso file',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jkmartindale/ccd2iso',
+    author='James Martindale',
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+    # entry_points={
+    #     'console_scripts': ['ccd2iso = ccd2iso:main'],
+    # }
+)
